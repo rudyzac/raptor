7b7665dd48b991304990b54f3f619ddbd748db08
Rudy Zaccaria <rudyzaccaria1@gmail.com>
2022-02-01 21:37:52 +0100

    wip

--- web-spa/src/pages/CustomerRegistration.jsx
+++ web-spa/src/pages/CustomerRegistration.jsx
@@ -3,14 +3,22 @@ import { useForm } from "react-hook-form"
 
 const CustomerRegistration = props => {
   const onSubmit = data => console.log(data)
-  const { register, handleSubmit } = useForm()
+  const { register, formState: { errors }, handleSubmit } = useForm()
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName")} placeholder="First name" />
-      <input {...register("lastName")} placeholder="Last name" />
-      <input {...register("email")} placeholder="Email" />
-      <input {...register("password")} placeholder="Password" />
+      <input {...register("firstName", { required: true })} placeholder="First name" />
+      {errors.firstName?.type === 'required' && "First name is required"}
+
+      <input {...register("lastName", { required: true })} placeholder="Last name" />
+      {errors.firstName?.type === 'required' && "Last name is required"}
+
+      <input {...register("email", { required: true })} placeholder="Email" />
+      {errors.firstName?.type === 'required' && "Email is required"}
+
+      <input {...register("password", { required: true })} placeholder="Password" />
+      {errors.firstName?.type === 'required' && "Password is required"}
+      
       <input type="submit" />
     </form>
   )
